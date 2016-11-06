2a821ca1ab1eb99d70e3eaa5d3168c0ac2d067f0
xmc <xiaomuchen2@gmail.com>
2016-11-06 19:35:40 +0800

    feat: 使用webpack-dev-server实时监听，并编译文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    devtool: 'eval-source-map',
+
     entry: './src/app.js',
     output: {
         path: './dist',
@@ -10,5 +12,13 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader'
         }]
+    },
+
+    devServer: {
+        contentBase: "./dist", //本地服务器所加载的页面所在的目录
+        colors: true, //终端中输出结果为彩色
+        hot: true,
+        historyApiFallback: true, //不跳转
+        inline: true //实时刷新
     }
 };
