0327319c1c72fcc2344e90068466f748013b1711
xmc <xiaomuchen2@gmail.com>
2016-11-15 15:03:32 +0800

    feat: hello koa

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('cccc')
+var koa = require('koa');
+var app = koa();
+var staticServer = require('koa-static')
+
+
+app.use( staticServer(__dirname + '/public/', {}) );
+
+
+
+app.listen(3000);
+console.log('open: http://127.0.0.1:3000')
